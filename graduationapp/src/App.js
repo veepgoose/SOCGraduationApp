068516d6313c89243fddaf020af8974f0ebaa6f5
Sam Baldwin <sambaldwin92@gmail.com>
2023-07-16 16:29:57 +0100

    Added useState array so users can save their responses as they go through the different forms

--- graduationapp/src/App.js
+++ graduationapp/src/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MondayStayForm from "./Components/Forms/MondayStayForm";
-import logo from './logo.svg';
-import './App.css';
-import soclogo from './socdows logo.png';
-import NameForm from './Components/Forms/NameForm';
+import "./App.css";
+import soclogo from "./socdows logo.png";
+import NameForm from "./Components/Forms/NameForm";
 
-function App() {
+function App()
+{
+  // State to keep track of which form we are on
   const [currentForm, setCurrentForm] = useState(1);
+  // State to keep track of the responses
+  const [response, setResponse] = useState([]);
 
-  const handleNextForm = () => {
+  // This will log the response to the console every time it changes
+  // Is for checking code works!
+  // Is not needed for functionality
+    useEffect(() => {
+      console.log(response[0]);
+    }, [response]);
+
+  // This function will be passed to the form components
+  const handleNextForm = (data) =>
+  {
+    // This will update the state variables
     setCurrentForm(currentForm + 1);
+    // This will add the response to the array
+    setResponse([...response, data]);
+    // This will log the response to the console
+    console.log("next button clicked");
   };
 
-  const renderForm = () =>
-  {
-    console.log('next button clicked')
-
+  const renderForm = () => {
     switch (currentForm) {
       case 1:
         return <NameForm onNext={handleNextForm} />;
@@ -28,12 +42,12 @@ function App() {
 
   return (
     <div className="App">
-    <div className="background-image">
-      <header className="App-header">
-      <img src={soclogo} className="App-logo" alt="logo" />
-      <header className="App-header">{renderForm()}</header>
-      </header>
-    </div>
+      <div className="background-image">
+        <header className="App-header">
+          <img src={soclogo} className="App-logo" alt="logo" />
+          <header className="App-header">{renderForm()}</header>
+        </header>
+      </div>
     </div>
   );
 }
