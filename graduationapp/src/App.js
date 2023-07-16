53b145417aa1e4a1864192484d17594949f208ba
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-07-16 12:48:14 +0100

    Soc logo added to App-Header

--- graduationapp/src/App.js
+++ graduationapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import soclogo from './socdows logo.png';
 import NameForm from './Components/Forms/NameForm';
 import DrinkPreferenceRadioButtons from './Components/Radios/DrinkPreferenceRadioButtons';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
     <div className="background-image">
       <header className="App-header">
+      <img src={soclogo} className="App-logo" alt="logo" />
       <NameForm />
       </header>
     </div>
