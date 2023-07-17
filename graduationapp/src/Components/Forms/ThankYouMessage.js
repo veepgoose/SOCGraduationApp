c76327028e69af5645cbb0b4863fc148aa42c859
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-07-17 09:11:01 +0100

    Thank you gif added - styling needed

--- graduationapp/src/Components/Forms/ThankYouMessage.js
+++ graduationapp/src/Components/Forms/ThankYouMessage.js
@@ -5,14 +5,13 @@ import Cards from "../Cards/Cards";
 
 function ThankYouMessage(props) {
  
-
-
-  return (
+    return (
     <div className="Body">
       <Cards>
         <h2 className="Form-Title">THANK YOU!</h2>
         <p className="Form-Text">
           "We'll be in touch!"
+          <img  className="gif" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmNvczhmNWl6b2Q3c2d2NnE2ZWUxamFwYXBybnR3d2F4ajdwZWdtYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Dh5q0sShxgp13DwrvG/giphy.gif" alt="Thank you" />
         </p>
         </Cards>
     </div>
