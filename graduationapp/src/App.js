b4fd1052b8ecdc1b1ba110ba6f88d74139806771
Goose <victoriaplatt@loveplanes.co.uk>
2023-07-18 20:36:52 +0100

    Continuing to wrestle with the backend...some progress made, but a ways to go! Tomorrow is a new day...

--- graduationapp/src/App.js
+++ graduationapp/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import React, { useState } from "react";
+import axios from "axios";
 import MondayStayForm from "./Components/Forms/MondayStayForm";
 import './App.css';
 import soclogo from './socdows logo.png';
@@ -21,10 +21,6 @@ const formSequence = [
   ThankYouMessage,
 ];
 
-const supabaseUrl = 'https://uoznfrtkyowzlhglmoac.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvem5mcnRreW93emxoZ2xtb2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzNDk4MzcsImV4cCI6MjAwNDkyNTgzN30.wFQPxadi9ieAB8zBhwGwRmrOQD6FoUnVX3I9v6M9BQI';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 function App() {
   const [currentFormIndex, setCurrentFormIndex] = useState(0);
   const [response, setResponse] = useState([]);
@@ -32,31 +28,18 @@ function App() {
   const handleNextForm = async (data) => {
     try {
       if (currentFormIndex === 0) {
-        const { data: attendeeResponse, error: attendeeError } = await supabase
-          .from('attendees')
-          .insert([{ name: data.name, email: data.email }]);
-            console.log(data, "supabase");
-        if (attendeeError) {
-          console.error('Error submitting attendee:', attendeeError);
-        } else {
-          console.log('Attendee submitted successfully:', attendeeResponse);
-        }
+        const { name, email } = data;
+        await axios.post('/attendees', { name, email });
+        console.log(data, "ElephantSQL");
       } else if (currentFormIndex === 5) {
-        const { data: drinkPreferenceResponse, error: drinkPreferenceError } = await supabase
-          .from('drink_preferences')
-          .insert([{ no_alcohol: data.none, beer: data.beer, wine: data.wine, spirits: data.spirits, beast_mode: data.beast }]);
-
-        if (drinkPreferenceError) {
-          console.error('Error submitting drink preferences:', drinkPreferenceError);
-        } else {
-          console.log('Drink preferences submitted successfully:', drinkPreferenceResponse);
-        }
+        const { none, beer, wine, spirits, beast } = data;
+        await axios.post('/drink-preferences', { no_alcohol: none, beer, wine, spirits, beast });
       }
 
       setCurrentFormIndex(currentFormIndex + 1);
       setResponse([...response, data]);
     } catch (error) {
-      console.error('Error submitting response:', error);
+      // console.error('Error submitting response:', error);
     }
   };
 
@@ -71,10 +54,8 @@ function App() {
       <div className="background-image">
         <header className="App-header">
           <img src={soclogo} className="App-logo" alt="logo" />
-          <header className="App-header">
-            <CurrentForm onNext={handleNextForm} />
-          </header>
         </header>
+        <CurrentForm onNext={handleNextForm} />
       </div>
     </div>
   );
