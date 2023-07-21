88ef4718661892666113eafcf6845e6d4f860632
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-07-21 23:40:15 +0100

    End screen and Goose Squad logo added. We are DONE! If we excuse the media queries, but the app works. WooHoo!

--- graduationapp/src/Components/Forms/ThankYouMessage.js
+++ graduationapp/src/Components/Forms/ThankYouMessage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Forms.css";
 import Cards from "../Cards/Cards";
+import GooseSquad from "./GooseSquadLogoTipsyApp.png"
 
 function ThankYouMessage(props) {
   return (
@@ -9,11 +10,9 @@ function ThankYouMessage(props) {
         <h2 className="Form-Title">🎉WOOHOO!🎉</h2>
         <p className="Form-Text">
         Thank you for taking part, and we can't wait to meet you all!
-          <img
-            className="gif"
-            src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmNvczhmNWl6b2Q3c2d2NnE2ZWUxamFwYXBybnR3d2F4ajdwZWdtYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Dh5q0sShxgp13DwrvG/giphy.gif"
-            alt="Thank you"
-          />
+          <img src={GooseSquad} className="GooseSquad" alt="GooseSquad" />
+          
+          
         </p>
       </Cards>
     </div>
